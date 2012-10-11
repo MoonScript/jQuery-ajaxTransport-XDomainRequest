5f2567086c5bc17a9319a041b7ea35235200b209
Jason Moon <jmoon@socialcast.com>
2012-10-11 12:48:36 -0500

    Send userOptions.data if it exists

--- jQuery.XDomainRequest.js
+++ jQuery.XDomainRequest.js
@@ -72,8 +72,7 @@ if (!jQuery.support.cors && window.XDomainRequest) {
 						});
 					};
 					xdr.open(options.type, options.url);
-					//xdr.send(userOptions.data);
-					xdr.send();
+					xdr.send(userOptions.data||null);
 				},
 				abort: function(){
 					if (xdr) {
