991a138e1ee90053a0566b538007504aab8f5125
Jorge <jorge@tvplus.com>
2012-11-13 00:12:23 -0800

    check the url in requests options instead of in the user options

--- jQuery.XDomainRequest.js
+++ jQuery.XDomainRequest.js
@@ -11,7 +11,7 @@ if (!jQuery.support.cors && window.XDomainRequest) {
   // ajaxTransport exists in jQuery 1.5+
   jQuery.ajaxTransport('text html xml json', function(options, userOptions, jqXHR){
     // XDomainRequests must be: asynchronous, GET or POST methods, HTTP or HTTPS protocol, and same scheme as calling page
-    if (options.crossDomain && options.async && getOrPostRegEx.test(options.type) && httpRegEx.test(userOptions.url) && sameSchemeRegEx.test(userOptions.url)) {
+    if (options.crossDomain && options.async && getOrPostRegEx.test(options.type) && httpRegEx.test(options.url) && sameSchemeRegEx.test(options.url)) {
       var xdr = null;
       var userType = (userOptions.dataType||'').toLowerCase();
       return {
