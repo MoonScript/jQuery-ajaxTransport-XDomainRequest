0e5c74a0c82707700a82073158d2adf0db4cd552
Devon Govett <devongovett@gmail.com>
2014-06-05 11:49:26 -0700

    Add CommonJS support

--- jQuery.XDomainRequest.js
+++ jQuery.XDomainRequest.js
@@ -8,6 +8,9 @@
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as anonymous module.
     define(['jquery'], factory);
+  } else if (typeof exports === 'object') {
+    // CommonJS
+    module.exports = factory(require('jquery'));
   } else {
     // Browser globals.
     factory(jQuery);
